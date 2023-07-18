288b38fe9938c874072ce375f360c436bf67eb4f
Esraa Elkabeer <esraaelkaber18@gmail.com>
2023-07-18 19:10:36 +0300

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface props {
+	score: number;
+}
+
+const CriticScore = ({ score }: props) => {
+	let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+	return (
+		<Badge fontSize="14px" padding={2} borderRadius="4px" colorScheme={color}>
+			{score}
+		</Badge>
+	);
+};
+
+export default CriticScore;
